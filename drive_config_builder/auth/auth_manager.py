f6ca950800b8da61d1710aace4649cca170e2b35
Nahom Garefo <harobina28@gmail.com>
2025-12-24 17:19:29 +0300

    authenticate google drive service and save token

---
+++ drive_config_builder/auth/auth_manager.py
@@ -0,0 +1,46 @@
+# │       - handles Google Drive authentication
+# │       - loads credentials.json
+# │       - returns authenticated Drive service object
+import os
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from googleapiclient.discovery import build
+
+
+SCOPES = ["https://www.googleapis.com/auth/drive.readonly"]
+
+def get_authenticated_drive_service():
+    """
+    Authenticates with Google Drive and returns a service client.
+    Handles token saving and refreshing automatically.
+    
+    Requires:
+        - credentials.json  (OAuth client ID from Google Cloud)
+    Produces:
+        - token.json        (saved automatically after first login)
+    """
+    creds = None
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            try:
+                
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    "credentials.json", SCOPES)
+                creds = flow.run_local_server(port=0)
+            except Exception as e:
+                print("Error during authentication:", e)
+                raise e
+        
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
+    
+    service = build("drive", "v3", credentials= creds )
+    return service
+
+get_authenticated_drive_service()
