299ccb8568069f472c2f99e4b1a6c7e56f1ea21c
Nahom Garefo <harobina28@gmail.com>
2025-12-28 20:03:02 +0300

    read a drive folder and file

---
+++ drive_config_builder/drive/drive_reader.py
@@ -0,0 +1,43 @@
+# │   │   - high-level functions:
+# │   │       get_folder_children(folder_id)
+# │   │       get_file_metadata(file_id)
+# │   │   - abstracts raw Google API requests
+from googleapiclient.errors import HttpError
+
+def list_children(service, folder_id):
+    '''
+    Lists all files and folders inside a given Google Drive folder.
+    
+    :param service: Authenticated Drive service object
+    :param folder_id: ID of the folder to list children from
+    
+    Returns: List of dictionaries with keys:
+            - id
+            - name
+            - mimeType
+    '''
+    try:
+        query = f"'{folder_id}' in parents and trashed = false"
+        results = service.files().list(
+            q = query,
+            fields = "files(id, name, mimeType)"
+        ).execute()
+        items = results.get('files', [])
+        return items
+    
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+        return []
+
+
+def get_file_url(file_id):
+    '''
+    Builds a direct URL to access a Google Drive view url for a file.
+    '''
+    return f"https://drive.google.com/file/d/{file_id}/view"
+
+def is_folder(item):
+    '''
+    Checks if a given item is a folder based on its mime type.
+    '''
+    return item.get("mimeType") == "application/vnd.google-apps.folder"
